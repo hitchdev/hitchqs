de39f9f51bced0194f42951b181ef6e2264b24f7
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-12-04 13:33:12 +0000

    FEATURE : Added documentation to skeleton key.

--- hitchqs/templates/skeleton/key/key.py
+++ hitchqs/templates/skeleton/key/key.py
@@ -1,11 +1,27 @@
-from commandlib import CommandError, Command
+from commandlib import CommandError, Command, python, python_bin
 from hitchrun import DIR, expected
 
 
+# If "expected" is used, no stacktrace will be displayed for that exception
 @expected(CommandError)
-def helloworld():
+def hello(argument):
     """
-    Say hello world.
+    Try running "hk hello world".
     """
-    DIR.gen.joinpath("hello.txt").write_text("Hello world")
+    DIR.gen.joinpath("hello.txt").write_text(argument)
     Command("cat", "hello.txt").in_dir(DIR.gen).run()
+
+
+## path.py objects -- 
+
+# DIR.gen -- build directory (~/.hitch/xxxxxx, where the symlink 'gen' links to)
+# DIR.project -- the directory containng the "hitch" folder.
+# DIR.key -- the directory this file - key.py is in.
+# DIR.share -- ~/.hitch/share - build folder shared build artefacts.
+# DIR.cur -- the directory "hk" was run in.
+
+## Command objects -- 
+
+# Command("cat").run() -- runs cat
+# python("pythoncode.py").run() -- run python code using this project's virtualenv
+# python_bin.python("pythoncode.py").run() -- equivalent to above. Easily run CLI commands this way.
